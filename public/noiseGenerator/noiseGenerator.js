5cfc39178c9a8a89ef62cfa50da12203b9abf9e6
jack-cedar <78448269+jack-cedar@users.noreply.github.com>
2021-03-29 17:58:29 -0700

    commit

---
+++ public/noiseGenerator/noiseGenerator.js
@@ -0,0 +1,17 @@
+var canvas = document.querySelector('canvas');
+var canvas = document.getElementById('myCanvas');
+var c = canvas.getContext('2d');
+let height = document.getElementById('myCanvas').height;
+let width = document.getElementById('myCanvas').width;
+function clearCanvas(){c.clearRect(0,0,width,height);}
+function generate(){
+    var tiles =document.getElementById("size").value;
+    var tileSize = parseFloat(tiles);
+    for (var x = 0; x < width/tileSize*tileSize; x+=tileSize){
+        for (var y = 0; y < height/tileSize*tileSize; y+=tileSize){                    
+            let v = Math.random()
+            c.globalAlpha = v;
+        c.fillRect(x , y, tileSize, tileSize);
+        }
+    }
+}
