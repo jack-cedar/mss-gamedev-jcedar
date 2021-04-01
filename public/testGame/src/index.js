d08fe75fef1b9f6d88f3b6f6bdaf249355298845
jack-cedar <78448269+jack-cedar@users.noreply.github.com>
2021-04-01 16:05:54 -0700

    commit

---
+++ public/testGame/src/index.js
@@ -0,0 +1,10 @@
+import player from './player'
+let canvas = document.getElementById("gameDisplay")
+let ctx = canvas.getContext("2d");
+const gameWidth = 800;
+const gameHeight = 600;
+
+
+ctx.clearRect(0, 0, 800, 600)
+let player = new player(gameWidth, gameHeight);
+player.draw(ctx);
---
+++ public/testGame/src/player.js
@@ -0,0 +1,14 @@
+export default class player {
+    constructor(gameWidth, gameHeight){
+        this.width=50;
+        this.height=100;
+
+        this.position = {
+            x: gameWidth/2 -this.width / 2,
+            y:hameHeight = this.height - 10,
+        }
+    }
+    draw(ctx) {
+        ctx.fillRect(this.position.x, this.position.y, this.width, this.height)
+    }
+}
