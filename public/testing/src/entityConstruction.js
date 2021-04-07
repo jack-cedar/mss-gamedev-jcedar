2fbdb1e271a65300b5ef0a5e9ffc8b55a8150163
jack-cedar <78448269+jack-cedar@users.noreply.github.com>
2021-04-07 12:26:05 -0700

    commit

---
+++ public/testing/src/entityConstruction.js
@@ -0,0 +1,7 @@
+import entity from 'src/entityConstructor.js';
+let canvas = document.getElementById("display");
+let ctx = canvas.getContext('2d');
+const  DISPLAY_WIDTH = 800;
+const  DISPLAY_HEIGHT = 600;
+
+let player01 = new entity(DISPLAY_WIDTH, DISPLAY_HEIGHT)
---
+++ public/testing/src/gameLoop.js
@@ -0,0 +1,22 @@
+let canvas = document.getElementById("display");
+let ctx = canvas.getContext('2d');
+var playerX = 20;
+var playerY = 20;
+function update(dTime){
+    if (!d) return;
+    playerX += 5 / d
+}
+function draw(){
+    player01 = ctx.fillRect(playerX,playerY,100,100)
+}
+let pTime = 0;
+
+function gameLoop(cTime) {
+    let dTime = cTime - pTime;
+    pTime = cTime;
+    ctx.clearRect(0,0,800,600);
+    update(d);
+    draw();
+    requestAnimationFrame(gameLoop); 
+}
+gameLoop();
