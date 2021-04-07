5e14c969dc33de87c482bf28771c59418b91e360
jack-cedar <78448269+jack-cedar@users.noreply.github.com>
2021-04-07 12:36:07 -0700

    commit

--- public/testing/src/entityConstruction.js
+++ public/testing/src/entityConstruction.js
@@ -4,4 +4,5 @@ let ctx = canvas.getContext('2d');
 const  DISPLAY_WIDTH = 800;
 const  DISPLAY_HEIGHT = 600;
 
-let player01 = new entity(DISPLAY_WIDTH, DISPLAY_HEIGHT)
+let player01 = new entity(DISPLAY_WIDTH, DISPLAY_HEIGHT);
+player01.draw(ctx);
