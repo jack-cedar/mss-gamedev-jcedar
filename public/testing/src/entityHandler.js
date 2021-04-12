234a846e193b95e4f85d5f5669b2312858c315e1
jack-cedar <78448269+jack-cedar@users.noreply.github.com>
2021-04-12 10:04:04 -0700

    commit

--- public/testing/src/entityHandler.js
+++ public/testing/src/entityHandler.js
@@ -4,22 +4,16 @@ export default class entity {
         this.height = newEntity.height
         this.xpos = newEntity.xpos
         this.ypos = newEntity.ypos
-        this.Vx = 0
-        this.Vy = 0
-        this.Vt = 10
+        this.Vx = newEntity.xVel
+        this.Vy = newEntity.yVel
     }
     draw(ctx){
         ctx.fillRect(this.xpos, this.ypos, this.width, this.height);
     }
-    move(){
-        left: this.Vx -= this.Vt;
-    }
-    update(dTime, newEntity){
+    update(dTime){
         if(!dTime) return;
-        this.xpos += this.Vx;
-        this.ypos += this.Vy;
-        this.xpos += newEntity.xpos;
-        this.ypos += newEntity.ypos;
+        this.xpos += this.Vx/dTime;
+        this.ypos += this.Vy/dTime;       
     }
 }
 
--- public/testing/src/main.js
+++ public/testing/src/main.js
@@ -3,21 +3,26 @@ let canvas = document.getElementById("display");
 let ctx = canvas.getContext('2d');
 const  DISPLAY_WIDTH = document.getElementById("display").width;
 const  DISPLAY_HEIGHT = document.getElementById("display").height;
-document.getElementById("constructEntity").addEventListener('click', createNewEntity);
+ let createButton = document.getElementById("constructEntity")
+ createButton.addEventListener('click', createNewEntity);
 
-var newEntity = {
-name: document.getElementById("entityName").value,
+let newEntity = {
 xpos: parseInt(document.getElementById("entityXpos").value),
 ypos: parseInt(document.getElementById("entityYpos").value),
 width: parseInt(document.getElementById("entityWidth").value),
 height: parseInt(document.getElementById("entityHeight").value),
+xVel: parseInt(document.getElementById("xVel").value),
+yVel: parseInt(document.getElementById("yVel").value),
 }
-function createNewEntity(){
-    newEntity.name = document.getElementById("entityName").value;
+function createNewEntity(newEntity){
     newEntity.xpos = parseInt(document.getElementById("entityXpos").value);
     newEntity.ypos = parseInt(document.getElementById("entityYpos").value);
     newEntity.width = parseInt(document.getElementById("entityWidth").value);
     newEntity.height = parseInt(document.getElementById("entityHeight").value);
+    newEntity.xVel = parseInt(document.getElementById("xVel").value);
+    newEntity.yVel = parseInt(document.getElementById("yVel").value);
+    player01 = new entity(newEntity);
+    gameLoop();
 }
 let player01 = new entity(newEntity);
 let pTime = 0;
@@ -25,9 +30,9 @@ function gameLoop(cTime) {
     let dTime = cTime - pTime;
     pTime = cTime;
     ctx.clearRect(0, 0, DISPLAY_WIDTH, DISPLAY_HEIGHT)
-    player01.update(dTime, newEntity);
     player01.draw(ctx);
+    player01.update(dTime);
     requestAnimationFrame(gameLoop); 
 }
-gameLoop();
+
 
