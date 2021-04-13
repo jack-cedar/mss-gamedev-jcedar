06afdd1d9ce2cf64aba8535d6b9ba93d15e50fbc
jack-cedar <jjanousekw@gmail.com>
2021-04-13 14:20:07 -0700

    commit

--- public/finalProject/src/main.js
+++ public/finalProject/src/main.js
@@ -2,5 +2,5 @@ import {player} from './player.js'
 import {enemy} from './enemy.js'
 let rat = new enemy(10, 2)
 let player01 = new player()
-player01.attack(rat)
+player01.attack(rat.hp)
 console.log(rat)
