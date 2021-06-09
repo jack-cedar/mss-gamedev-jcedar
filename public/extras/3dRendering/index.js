0af70bce8c4e19a4705f3a03ba3d47012b7b2d35
jack-cedar <78448269+jack-cedar@users.noreply.github.com>
2021-06-09 15:22:19 -0700

    commit

--- public/extras/3dRendering/index.js
+++ public/extras/3dRendering/index.js
@@ -4,13 +4,9 @@ let width = canvas.width = window.innerWidth;
 let height = canvas.height = window.innerHeight;
 ctx.strokeStyle = "white";
 
-
 var grd = ctx.createLinearGradient(0, 200, 200, 0);
 grd.addColorStop(0, "red");
 grd.addColorStop(1, "blue");
-
-// Fill with gradient
-ctx.fillStyle = grd;
 
 let angle = 0;
 var points = []
@@ -37,7 +33,6 @@ points = [
   new point(  size,  size, size),
   new point( -size,  size, size),
 ]
-
 function matrixifyPoints(points){
   var pointMatrix = [];
   for (i = 0; i < points.length; i++){
@@ -52,7 +47,6 @@ let projection = [
   [0, 1, 0],
   [0, 0, 0]
 ]
-
 let rotationX = [
   [1, 0, 0],
   [0, Math.cos(angle), -Math.sin(angle)],
@@ -70,14 +64,13 @@ let rotationZ = [
 ]
 function projectPoints(){
   for(var i = 0; i < points.length; i++){
-    var rotated = multiplyMatrices(pointMatrix, rotationZ)
-    rotated = multiplyMatrices(rotated, rotationX)
+    var rotated = multiplyMatrices(pointMatrix, rotationX)
     rotated = multiplyMatrices(rotated, rotationY)
+    rotated = multiplyMatrices(rotated, rotationZ)
     var Projected2dPoint = multiplyMatrices(rotated, projection)
     points[i].sx = Projected2dPoint[i][0];
     points[i].sy = Projected2dPoint[i][1];
 }
-
 }
 function drawPoints(){
   ctx.fillStyle = "white";
@@ -92,17 +85,17 @@ function render(){
   ctx.fillStyle = grd;
   ctx.beginPath()
   drawPolygon(0, 1, 2, 3, 0)
-  ctx.fill();
+  //ctx.fill();
   drawPolygon(4, 5, 6, 7, 4)
-  ctx.fill();
+  //ctx.fill();
   drawPolygon(0, 1, 5, 4, 0)
-  ctx.fill();
+  //ctx.fill();
   drawPolygon(1, 5, 6, 2, 1)
-  ctx.fill();
+  //ctx.fill();
   drawPolygon(0, 3, 7, 4, 0)
-  ctx.fill();
+  //ctx.fill();
   drawPolygon(6, 7, 3, 2, 6)
-  ctx.fill();
+  //ctx.fill();
   ctx.stroke();
 }
 function drawPolygon(){
@@ -112,10 +105,6 @@ function drawPolygon(){
     p = points[arguments[i]];
     ctx.lineTo(p.sx, p.sy);
 }}
-
-
-
-
 function update(){
     ctx.clearRect(-width / 2, -height / 2, width, height);
     updateAngle()
@@ -127,8 +116,6 @@ function update(){
     requestAnimationFrame(update);    
 }
 update();
-
-
 function updateAngle(){
   rotationX = [
     [1, 0, 0],
